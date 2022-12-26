f22fbc560abafc0137f87b422c8af30f2f9704c7
Richard Hyde <email@richardhyde.net>
2022-12-26 13:31:36 +0000

    Added options.lua

---
+++ lua/options.lua
@@ -0,0 +1,39 @@
+local opt = vim.opt
+
+-- line numbering
+opt.relativenumber = true
+opt.number = true
+
+-- tabs
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- Search options
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Colours
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- Syntax highlighting
+opt.syntax = "on"
+
+-- backspace key
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+-- splits
+opt.splitright = true
+opt.splitbelow = true
+
+opt.iskeyword:append("-")
