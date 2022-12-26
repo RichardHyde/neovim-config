6728132be2be67f1ac280f595495650fcff79ea9
Richard Hyde <email@richardhyde.net>
2022-12-26 14:58:21 +0000

    Packer plugin setup

--- init.lua
+++ init.lua
@@ -1,3 +1,3 @@
+require("plugins-setup")
 require("options")
 require("keymaps")
-
---
+++ lua/plugins-setup.lua
@@ -0,0 +1,28 @@
+-- auto install packer if not installed
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
+end
+local packer_bootstrap = ensure_packer() -- true if packer was just installed
+
+-- import packer safely
+local status, packer = pcall(require, "packer")
+if not status then
+  return
+end
+
+-- add list of plugins to install
+return packer.startup(function(use)
+  -- packer can manage itself
+  use("wbthomason/packer.nvim")
+
+  if packer_bootstrap then
+      require("packer").sync()
+  end
+ end)
